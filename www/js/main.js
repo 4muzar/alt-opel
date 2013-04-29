9d6b3640cd66909df9f1783a0491bf6c2468b307
Tolya Krasotkin <juumanji@gmail.com>
2013-04-29 16:46:06 +0400

    главная и автосервис

---
+++ www/js/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+	var autoParts = $(".main_menu_item.auto-parts");
+	var dropdownMenu = $(".dropdown-menu");
+
+	autoParts.mouseover(function (e) {
+		dropdownMenu.css("display", "block");
+	});	
+
+	autoParts.mouseout(function (e) {
+		dropdownMenu.css("display", "none");
+	});
+});
