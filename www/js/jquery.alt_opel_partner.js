626d039ed48c5f5b374d59560ae433cf9cb6f8ce
Tolya Krasotkin <juumanji@gmail.com>
2013-06-20 18:37:54 +0400

    партнеры

---
+++ www/js/jquery.alt_opel_partner.js
@@ -0,0 +1,71 @@
+﻿(function( $ ){	
+	var methods = {
+		init : function( options ) {
+ 			return this.each(function(){
+				
+				var $this = $(this);
+				var data = $this.data('alt_opel_partner');
+			
+				if ( !data ) {
+					var settings = {
+					}
+					if ( options ) { 
+						$.extend( settings, options );
+					}
+					
+					data = {
+						settings 		: settings,
+						show			: null,
+						hide			: null
+					};
+					$this.data('alt_opel_partner', data);					
+					
+					var bullet = $this.find(".partner-header-bullet");
+					var body = $this.find(".partner-body");
+					
+					function show () {
+						$(".partner-block").each(function () {
+							$(this).data("alt_opel_partner").hide();
+						});
+						$this.removeClass("collapsed").addClass("expanded");
+						body.slideDown();						
+					}
+					data.show = show;
+					
+					function hide () {
+						$this.removeClass("expanded").addClass("collapsed");
+						body.slideUp();						
+					}
+					data.hide = hide;
+					
+					bullet.click(function (e) {
+						if ($this.hasClass("collapsed")) {
+							show();
+						} else {
+							hide();
+						}
+						
+						if (e && e.preventDefault) {
+							e.preventDefault();
+						}
+						return false;
+					});
+				}
+			});
+		}
+	};
+ 
+	$.fn.alt_opel_partner = function( method ) {
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.alt_opel_partner' );
+		}    
+	};
+ })( jQuery );
+ 
+ $(document).ready(function() {
+	$(".partner-block").alt_opel_partner();
+ });
